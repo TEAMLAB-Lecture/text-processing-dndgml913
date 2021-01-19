892d6f102602e5d0fa1b88bbe32429157f9d73c5
dndgml913 <dndgml913@naver.com>
2021-01-19 22:21:50 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -59,5 +59,5 @@ def no_vowels(input_string):
             ''W lv Pythn!'
     """
     no_vowel_string = input_string[:]
-    table = str.maketrans('aeiou', '') # https://dojang.io/mod/page/view.php?id=2299
+    table = str.maketrans('aeiou', '\0\0\0\0\0') # https://dojang.io/mod/page/view.php?id=2299
     return no_vowel_string.translate(table)
