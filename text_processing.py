257c20f4aeeca59688c37d8ae06bd665acdbf399
dndgml913 <dndgml913@naver.com>
2021-01-19 22:54:23 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -59,5 +59,5 @@ def no_vowels(input_string):
             ''W lv Pythn!'
     """
     no_vowel_string = input_string[:]
-    table = no_vowel_string.maketrans("", "", "aeiouAEIOU") # https://www.w3schools.com/python/ref_string_maketrans.asp
+    table = no_vowel_string.maketrans("", "", "aeioun") # https://www.w3schools.com/python/ref_string_maketrans.asp
     return no_vowel_string.translate(table)
